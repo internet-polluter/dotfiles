3173e85d0182ba08e1b3a36a7f0f7bce53fa3d7a
internet-polluter <internet0polluter@gmail.com>
2025-05-28 15:23:07 -0600

    unnamedplus clipboard

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,8 @@
 -- Load configuration
 require("config.lazy")
 
+vim.opt.clipboard = 'unnamedplus'
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlifht when yanking text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
