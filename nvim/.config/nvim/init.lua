9edfd9e9026260bf5676764c56e0dca84364cdd5
internet-polluter <internet0polluter@gmail.com>
2025-05-28 15:08:00 -0600

    highlight yank & lua language opts

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,10 @@
 -- Load configuration
 require("config.lazy")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlifht when yanking text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
