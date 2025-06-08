1edfde838eb7d725935b775e56d5a285c5ff617c
internet-polluter <internet0polluter@gmail.com>
2025-06-08 03:27:12 -0600

    more telescope keybinds

--- nvim/.config/nvim/lua/config/keybinds.lua
+++ nvim/.config/nvim/lua/config/keybinds.lua
@@ -1,23 +1,46 @@
+-- This file is for your own custom keymaps.
+-- The "desc" key is used by which-key.nvim to show a description of the keymap.
+
 local map = vim.keymap.set
 
+-- [[ Editor Basics ]]
+
 -- Escape from insert mode with jk or kj
-vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
-vim.keymap.set("i", "kj", "<Esc>", { desc = "Exit insert mode" })
+map("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+map("i", "kj", "<Esc>", { desc = "Exit insert mode" })
 
--- Other useful keymaps
-vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
-vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
+-- Save and Quit
+map("n", "<leader>w", ":w<CR>", { desc = "[W]rite file" })
+map("n", "<leader>q", ":q<CR>", { desc = "[Q]uit" })
 
+-- [[ Telescope ]]
+-- Note: The require('telescope.builtin') is wrapped in a function,
+-- so it is only called when you press the keymap.
 
-map(
-  "n",
-  "<leader>fc",
-  function()
-    require("telescope.builtin").find_files({
-      -- vim.fn.stdpath("config") gets the path to your nvim config directory
-      search_dirs = { vim.fn.stdpath("config") },
-      prompt_title = "Neovim Config Files",
-    })
-  end,
-  { desc = "[F]ind [C]onfig File" }
-)
+-- Find files
+map("n", "<leader>ff", function()
+  require("telescope.builtin").find_files()
+end, { desc = "[F]ind [F]iles" })
+
+-- Find files in your Neovim config
+map("n", "<leader>fc", function()
+  require("telescope.builtin").find_files({
+    search_dirs = { vim.fn.stdpath("config") },
+    prompt_title = "Neovim Config Files",
+  })
+end, { desc = "[F]ind [C]onfig File" })
+
+-- Grep for a string in the current project
+map("n", "<leader>fg", function()
+  require("telescope.builtin").live_grep()
+end, { desc = "[F]ind by [G]rep" })
+
+-- List open buffers
+map("n", "<leader>fb", function()
+  require("telescope.builtin").buffers()
+end, { desc = "[F]ind [B]uffers" })
+
+-- Search help tags
+map("n", "<leader>fh", function()
+  require("telescope.builtin").help_tags()
+end, { desc = "[F]ind [H]elp Tags" })
